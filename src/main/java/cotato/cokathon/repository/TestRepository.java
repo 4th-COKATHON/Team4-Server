4429c1654a90ef019c587614cacd0996dd52322a
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2024-07-26 21:20:01 +0900

    first commit

---
+++ src/main/java/cotato/cokathon/repository/TestRepository.java
@@ -0,0 +1,10 @@
+package cotato.cokathon.repository;
+
+import cotato.cokathon.entity.TestEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestRepository extends JpaRepository<TestEntity, Long> {
+
+}
